4e73341967d9a6fe26b362585b53d9a08e678d43
Seth Digel <digel@stanford.edu>
2023-05-18 11:03:18 -0700

    Added and updated some config files, README.md

--- python/index_steps.py
+++ python/index_steps.py
@@ -4,18 +4,18 @@
 
 import glob
 
-outfile = '../Run5/README.md'
-link_root = 'https://github.com/lsst-camera-dh/bot-eotest-configs/blob/master/Run5/'
+outfile = '../Run6/README.md'
+link_root = 'https://github.com/lsst-camera-dh/bot-eotest-configs/blob/master/Run6/'
 
-files = glob.glob('../Run5/*/*.cfg') + glob.glob('../Run5/*/*/*.cfg')
+files = glob.glob('../Run6/*/*.cfg') + glob.glob('../Run6/*/*/*.cfg')
 files.sort()
 
 f = open(outfile,'w')
 
-f.write('# Index of Run5 configuration files\n')
+f.write('# Index of Run6 configuration files\n')
 print('# Index of configuration files')
 for file in files:
-    entry = file.split('Run5/')[1]
+    entry = file.split('Run6/')[1]
     entry_esc = entry.replace('_', '\_')
     f.write('* [' + entry_esc + '](' + link_root + entry + ')\n')
     print('* [' + entry_esc + '](' + link_root + entry + ')')
