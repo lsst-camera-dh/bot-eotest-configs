ad56edbf62ab3f66ca75fb730edac9087d490a8e
Sean MacBride <sean.p.macbride@gmail.com>
2024-08-20 18:56:32 -0400

    Changing to Run7 in README.md

--- python/index_steps.py
+++ python/index_steps.py
@@ -7,15 +7,15 @@ import glob
 outfile = '../Run7/README.md'
 link_root = 'https://github.com/lsst-camera-dh/bot-eotest-configs/blob/master/Run7/'
 
-files = glob.glob('../Run6/*/*.cfg') + glob.glob('../Run6/*/*/*.cfg')
+files = glob.glob('../Run7/*/*.cfg') + glob.glob('../Run7/*/*/*.cfg')
 files.sort()
 
 f = open(outfile,'w')
 
-f.write('# Index of Run6 configuration files\n')
+f.write('# Index of Run7 configuration files\n')
 print('# Index of configuration files')
 for file in files:
-    entry = file.split('Run6/')[1]
+    entry = file.split('Run7/')[1]
     entry_esc = entry.replace('_', '\_')
     f.write('* [' + entry_esc + '](' + link_root + entry + ')\n')
     print('* [' + entry_esc + '](' + link_root + entry + ')')
